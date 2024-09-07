3a1c215a92ad7cfcda3f02684aa1d90959d024c0
dendrocat <dan.re0@mail.ru>
2024-09-07 08:55:28 +0300

    Delete comments console.logs and add characters in characters

--- js/bug.js
+++ js/bug.js
@@ -9,13 +9,11 @@ function random(l, r) {
 }
 
 function doBug(prob) {
-    // console.log(random(0, 100))
     return random(0, 100) <= prob;
 }
 
 export function checkInBin() {
     if (!doBug(probCheck)) return;
-    //console.log("bug!");
 
     window.location = "profile.html";
     return true;
@@ -24,31 +22,27 @@ export function checkInBin() {
 export function rotateImage(img) {
     bugImage(img);
     if (!doBug(probRotate)) return;
-    //console.log("rot");
 
     img.style.transform = `rotate(${random(30, 300)}deg)`
 }
 
 export function bugCount(count) {
     if (!doBug(probCount)) return count;
-    //console.log("bugSUm");
 
     return count + 1
 }
 
 export function bugImage(img) {
     if (!doBug(probImg)) return;
-    //console.log("invert")
 
     img.style.filter = `invert(${random(0, 100)}%)` 
 }
 
 export function bugFilter() {
-    //console.log("filter")
     return doBug(probFilt)
 }
 
-const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_+-=?[]()';
 
 export function bugInput(input) {
     if (input.value.length == 0) return;
